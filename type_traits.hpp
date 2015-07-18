99e86d7fd5a4fb8dbeb127d4110e0cba1218a867
Ryan Domigan <nothingisbad@gmail.com>
2015-07-18 13:21:40 -0400

    Use new code style in type_traits

--- type_traits.hpp
+++ type_traits.hpp
@@ -8,29 +8,37 @@
 
 #include "./type.hpp"
 
-namespace atl {
-    namespace type_mapping {
-	template<class T>
-	struct atl_builtin {
-	    static_assert(atl::is_atl_type<T>::value, "No ATL type equivalent");
-	    typedef T type;
-	};
-
-	template<class Input>
-	struct cxx_to_atl
-	    : std::conditional< std::is_integral<Input>::value,
-				Identity<Fixnum>,
-				atl_builtin<Input>
-				>::type {};
-
-	template<> struct cxx_to_atl<bool> : public Identity<Bool> {};
-	template<> struct cxx_to_atl<int> : public Identity<Fixnum> {};
-	template<> struct cxx_to_atl<std::string> : public Identity<String> {};
+namespace atl
+{
+	namespace type_mapping
+	{
+		template<class T>
+		struct atl_builtin
+		{
+			static_assert(atl::is_atl_type<T>::value, "No ATL type equivalent");
+			typedef T type;
+		};
 
 
-	template<class T> struct unwrappable_type : public is_atl_type<T> {};
-	template<> struct unwrappable_type<std::string> : std::true_type {};
-    }
+		struct cxx_to_atl
+		{
+			template<class Input>
+			struct Apply
+				: std::conditional< std::is_integral<Input>::value,
+				                    Identity<Fixnum>,
+				                    atl_builtin<Input>
+				                    >::type
+			{};
+		};
+
+		template<> struct cxx_to_atl::Apply<bool> : public Identity<Bool> {};
+		template<> struct cxx_to_atl::Apply<int> : public Identity<Fixnum> {};
+		template<> struct cxx_to_atl::Apply<std::string> : public Identity<String> {};
+
+
+		template<class T> struct unwrappable_type : public is_atl_type<T> {};
+		template<> struct unwrappable_type<std::string> : std::true_type {};
+	}
 }
 
 #endif
